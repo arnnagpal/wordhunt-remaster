c5bd4e3402926579b697e2fda2ff13e0b0081e35
Aryan Nagpal <arnnagpal@outlook.com>
2024-10-20 02:38:59 -0700

    fix commonjs err

--- utils/src/dictionary/letters.ts
+++ utils/src/dictionary/letters.ts
@@ -1,5 +1,10 @@
 import { readFile } from "fs/promises";
 import { dictionary, letterFrequency } from "./dictionary";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
 export async function loadDictionary() {
     const dictionaryFile = await readFile(
